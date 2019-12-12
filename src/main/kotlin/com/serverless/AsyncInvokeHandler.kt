cff1459afd05be77b3085073bb29de79615bb378
takeda_san <takedasan593@gmail.com>
2019-12-13 01:28:52 +0900

    init

---
+++ src/main/kotlin/com/serverless/AsyncInvokeHandler.kt
@@ -0,0 +1,32 @@
+package com.serverless
+
+import com.amazonaws.services.lambda.AWSLambdaClientBuilder
+import com.amazonaws.services.lambda.model.InvokeRequest
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import org.apache.logging.log4j.LogManager
+
+class AsyncInvokeHandler : RequestHandler<Map<String, Any>, ApiGatewayResponse> {
+    override fun handleRequest(input: Map<String, Any>, context: Context): ApiGatewayResponse {
+        LOG.info("received: ${input.keys}")
+
+        val lambda = AWSLambdaClientBuilder.defaultClient()
+        val response = lambda.invoke(
+                InvokeRequest()
+                        .withFunctionName("retry-sync-lambda-dev-retrySync")
+                        .withInvocationType("Event")
+        )
+
+        LOG.info("response: ${response.statusCode}")
+
+        return ApiGatewayResponse.build {
+            statusCode = 200
+            objectBody = HelloResponse("Go Serverless v1.x! Your Kotlin function executed successfully!", input)
+            headers = mapOf("X-Powered-By" to "AWS Lambda & serverless")
+        }
+    }
+
+    companion object {
+        private val LOG = LogManager.getLogger(AsyncInvokeHandler::class.java)
+    }
+}
